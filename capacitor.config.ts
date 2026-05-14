bc1b84a4208ca1383cc92d5343c33a8f86ed6dcc
drewkrotz-tech <drewkrotz@gmail.com>
2026-05-14 14:51:32 -0400

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.sinistertrivia.macabretest',
-  appName: 'Macabre Test',
+  appName: 'The Dread Directory',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
